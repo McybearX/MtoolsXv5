38034dbac878b52e3bdfc1204905aaee91b2d692
Mcybear× <95560890+McybearX@users.noreply.github.com>
2022-03-17 00:27:22 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 pkg update -y
 pkg install mpv -y
 pkg install gtts -y
-pkg install transgoogle -y
+pkg install googletrans -y
 pkg install play-audio -y
 pkg install python2 -y
 pkg install python -y
